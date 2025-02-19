41d4a0e4ea8a029409f56034b01814aae16d4d33
Bruno Paiva <bruno.paiva@consultor.redspark.io>
2025-02-19 19:09:28 -0300

    fix: correção localStorage not found

--- frontend/src/app/guards/auth.guard.ts
+++ frontend/src/app/guards/auth.guard.ts
@@ -6,13 +6,14 @@ import { AuthService } from '../services/auth/auth.service';
   providedIn: 'root'
 })
 export class AuthGuard implements CanActivate {
+
   constructor(private authService: AuthService, private router: Router) {}
 
   canActivate(): boolean {
-    if (!this.authService.isAuthenticated()) {
-      this.router.navigate(['/login']);
-      return false;
+    if (this.authService.isAuthenticated()) {
+      return true;
     }
-    return true;
+    this.router.navigate(['/login']);
+    return false;
   }
 }
