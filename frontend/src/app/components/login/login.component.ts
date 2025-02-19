6e59523e0bf5a369e73b508a75b6bdab6a0f82f9
Bruno Paiva <bruno.paiva@consultor.redspark.io>
2025-02-19 16:41:18 -0300

    feat: implementando integração com web-api

---
+++ frontend/src/app/components/login/login.component.ts
@@ -0,0 +1,31 @@
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  imports: [FormsModule],
+  standalone: true,
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+  username = '';
+  password = '';
+  errorMessage = '';
+
+  constructor(private authService: AuthService, private router: Router) {}
+
+  login(): void {
+    this.authService.login(this.username, this.password).subscribe({
+      next: (response) => {
+        this.authService.setToken(response.token);
+        this.router.navigate(['/chat']); // Redireciona após login bem-sucedido
+      },
+      error: () => {
+        this.errorMessage = 'Credenciais inválidas. Tente novamente.';
+      }
+    });
+  }
+}
