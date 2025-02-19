9cedafd940357ca0abba21eec3ee0d28961ed7b5
Bruno Paiva <bruno.paiva@consultor.redspark.io>
2025-02-19 18:18:17 -0300

    feat: implementação do styling inicial

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -1,14 +1,14 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { authInterceptor } from './interceptors/auth-interceptor.service';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideHttpClient(withInterceptors([authInterceptor])),
-    provideRouter(routes), provideClientHydration()
+    provideRouter(routes), provideClientHydration(), provideAnimationsAsync()
   ],
 };
